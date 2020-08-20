9dc3094f67e347752860cf7b46fce1d38fceea65
10minsturnuon7time <44266907+10minsturnuon7time@users.noreply.github.com>
2020-08-20 13:36:53 +0800

    Add files via upload

---
+++ precache-manifest.f44a8def150282201237e1979a9b40af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "294ebedf73f11aa17cae",
+    "url": "./static/css/main.ef8bea2f.chunk.css"
+  },
+  {
+    "revision": "294ebedf73f11aa17cae",
+    "url": "./static/js/main.294ebedf.chunk.js"
+  },
+  {
+    "revision": "2f336dea24584514df59",
+    "url": "./static/js/1.2f336dea.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d410f78d207becd19c7b5916507a56cd",
+    "url": "./index.html"
+  }
+];
